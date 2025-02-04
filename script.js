9d78b5a788c56d69f4bebf81bed62dcda8dda0bc
Raheem. <166643761+lgndcraft2@users.noreply.github.com>
2025-02-04 19:11:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Function to display current UTC time
 function displayUTCTime() {
-    const timeZone = "Africa/Lagos"; // Example: Nigeria Timezone
+    const timeZone = "Africa/Lagos";
     const options = { timeZone, hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
     
     document.querySelector('[data-testid="currentTimeUTC"]').textContent = new Intl.DateTimeFormat('en-US', options).format(new Date());
   }
   
-  // Update UTC time on page load
-  displayUTCTime();
+displayUTCTime();
