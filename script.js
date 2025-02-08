ad61e92c6d627832893c25f4d8685b296243df6c
Raheem. <166643761+lgndcraft2@users.noreply.github.com>
2025-02-08 20:59:35 +0100

    Update script.js
    Added dynamic timer

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// Function to display current UTC time
 function displayUTCTime() {
-    const timeZone = "Africa/Lagos";
+    const timeZone = "Africa/Lagos"; // Example: Nigeria Timezone
     const options = { timeZone, hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
     
-    document.querySelector('[data-testid="currentTimeUTC"]').textContent = new Intl.DateTimeFormat('en-US', options).format(new Date());
+    setInterval(() => {
+      document.querySelector('[data-testid="currentTimeUTC"]').textContent = new Intl.DateTimeFormat('en-US', options).format(new Date());
+    }, 1000);
   }
   
-displayUTCTime();
+  // Update UTC time on page load
+  displayUTCTime();
